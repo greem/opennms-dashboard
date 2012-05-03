e7370cc42703f14efa090acd2120602013c7eb46
slm4996 <slm4996+git@gmail.com>
2012-05-03 13:28:11 -0400

    Updated recent outaged view with new look

--- content.php
+++ content.php
@@ -4,17 +4,17 @@ include "functions.php";
 <div class="left">
   <h2>Current Outages</h2>
   <div class="content">
-  <?
+  <?		  
   $outages = getOutages("?ifRegainedService=null&orderBy=ifRegainedService&order=desc&limit=100");
   foreach ($outages as $outage) {
     $services = "";
     foreach($outage['services'] as $service){
-        $services .= '<span class="bubble">'.$service['serviceName'].'</span>';
+	$services .= '<span class="bubble">'.$service['serviceName'].'</span>';
     }
     if (strstr($services, 'ICMP')) {
-        $severity = "CRITICAL";
+    	$severity = "CRITICAL";
     }else{
-        $severity = "MAJOR";
+    	$severity = "MAJOR";
     }
     echo '<div class="line '.$severity.'"><b>'.$outage["label"].'('.$outage["ipAddress"].')</b><p>'.$outage["ifLostService"].'</p><b>';
     echo $services;
@@ -25,31 +25,33 @@ include "functions.php";
   </div>
   <h2>Recent Outages</h2>
   <div class="content">
-  <?
+  <?		  
   $outages = getOutages("?ifRegainedService=notnull&orderBy=ifRegainedService&order=desc");
   foreach ($outages as $outage) {
-    echo '<div class="line NORMAL"><b>'.$outage["label"].'</b><p>';
-    foreach ($outage['services'] as $service) {
-        echo ' '.$service['serviceName'];
+    $services = "";
+    foreach($outage['services'] as $service){
+	$services .= '<span class="bubble">'.$service['serviceName'].'</span>';
     }
-    echo '</p></br>';
-    echo '<b>'.$outage["ipAddress"].'</b><p>'.$outage["ifRegainedService"].'</p><div class="clear"></div></div>';
-  }  
+    echo '<div class="line NORMAL"><b>'.$outage["label"].'('.$outage["ipAddress"].')</b><p>'.$outage["ifLostService"].'</p><b>';
+    echo $services;
+    echo '</b></br>';
+    echo '</p><div class="clear"></div></div>';
+  }
   ?>
   </div>
 </div>
 
 <div class="right">
-        <h2>Alarms</h2>
+	<h2>Alarms</h2>
   <div class="content" id="alarms">
-        <?
+	<?
   $alarms = getAlarms("?orderBy=firstEventTime&order=desc&limit=7");
   foreach ($alarms as $alarm) {
     print '<div class="line '.$alarm['severity'].'">';
-                print '<h4>'.$alarm['nodeLabel'].'</h4>';
-                print $alarm['description'];
-                print '<div class="clear"></div></div>';
-        }
+		print '<h4>'.$alarm['nodeLabel'].'</h4>';
+		print $alarm['description'];
+		print '<div class="clear"></div></div>';
+ 	}
   ?>
 </div>
 </div>
